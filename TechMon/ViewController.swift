1cdeef8e1e3725b334720a140cf2732aa6d5cb46
Miki Kimura <mikimucun0802@gmail.com>
2023-02-20 05:19:38 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 木村美希 on 2023/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
